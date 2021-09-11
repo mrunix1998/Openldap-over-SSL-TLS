074b1bd9f5671b046d4e66771efe39ffd99d2a73
mrunix1998 <mohammadsalehi31335@gmail.com>
2021-09-11 17:03:51 +0430

    Init first sourc

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+import subprocess
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print("""
+    1 - OpenLdap server without SSL/TLS
+    2 - OpenLdap client without SSL/TLS
+    3 - Openldap server with SSL/TLS
+    4 - OpenLdap client with SSL/TLS
+""")
+
+num = input("Enter Your choice : \n")
+distro = input("Enter Your release of ubuntu [focal | bionic | xenial] : ")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if num == '1':
+    if distro == 'focal':
+        print("Installing Openldap ...")
+        time.sleep(5)
+        subprocess.run("bash", "open")
+    elif distro == 'bionic':
+        pass
+    elif distro == 'xenial':
+        pass
+elif num == '2':
+    if distro == 'focal':
+        print("2-bionic")
+    elif distro == 'bionic':
+        pass
+    elif distro == 'xenial':
+        pass
+elif num == '3':
+    if distro == 'focal':
+        print("3-bionic")
+    elif distro == 'bionic':
+        pass
+    elif distro == 'xenial':
+        pass
+elif num == '4':
+    if distto == 'focal':
+        print("4-bionic")
+    elif distro == 'bionic':
+        pass
+    elif distro == 'xenial':
+        pass
---
+++ openldap.installer/README.md
@@ -0,0 +1,2 @@
+# openldap.installer
+auto install and configure openldap in ubuntu 20.04
