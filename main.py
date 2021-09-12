7f2eced4957e6a3e0aa6390613b7f4aaf13a1396
mrunix1998 <mohammadsalehi31335@gmail.com>
2021-09-12 16:47:29 +0430

    update source code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import shlex
 import subprocess, time
 from getpass import getpass
 
@@ -8,32 +10,43 @@ print("""
     4 - OpenLdap client with SSL/TLS
 """)
 
-num = input("Enter Your choice : \n")
+num = input("Enter Your choice : ")
 distro = input("Enter Your release of ubuntu [focal | bionic | xenial] : ")
 
 
 def generate_ssl(distro):
     try:
-        subprocess.run('cd /etc/ssl/private'.split())
-        subprocess.run('echo "# TLS_CACERT    /etc/ssl/certs/ca-certificate" > /etc/ldap/ldap.conf'.split(), encoding="ascii")
-        subprocess.run('echo "TLS_REQCERT    /etc/ssl/certs/ca-certificate" >> /etc/ldap/ldap.conf'.split(), encoding="ascii")
-        subprocess.run('openssl genrsa -aes128 -out server.key 2048'.split())
-        subprocess.run('openssl rsa -in server.key -out server.key'.split())
-        subprocess.run('openssl req -new -days 3650 -key server.key -out server.csr'.split())
-        subprocess.run('openssl x509 -in server.csr -out server.crt -req -signkey server.key -days 3650'.split())
+        os.popen('echo "#TLS_CACERT    /etc/ssl/certs/ca-certificate" > /etc/ldap/ldap.conf').read()
+        time.sleep(1)
+        print("config set")
+        os.popen('echo "TLS_REQCERT   /etc/ssl/certs/ca-certificate" >> /etc/ldap/ldap.conf').read()
+        print("config set")
+        time.sleep(1)
+        subprocess.run('openssl genrsa -aes128 -out /etc/ssl/private/server.key 2048'.split())
+        time.sleep(1)
+        subprocess.run('openssl rsa -in /etc/ssl/private/server.key -out /etc/ssl/private/server.key'.split())
+        time.sleep(1)
+        subprocess.run('openssl req -new -days 3650 -key /etc/ssl/private/server.key -out /etc/ssl/private/server.csr'.split())
+        time.sleep(1)
+        subprocess.run('openssl x509 -in /etc/ssl/private/server.csr -out /etc/ssl/private/server.crt -req -signkey /etc/ssl/private/server.key -days 3650'.split())
+        time.sleep(1)
         subprocess.run('cp /etc/ssl/private/{server.key,server.crt} /etc/ssl/certs/ca-certificates.crt /etc/ldap/sasl2/'.split())
+        time.sleep(1)
         subprocess.run('chown openldap. /etc/ldap/sasl2/server.key /etc/ldap/sasl2/server.crt /etc/ldap/sasl2/ca-certificates.crt'.split())
+        time.sleep(1)
         if distro == 'xenial':
             subprocess.run('bash command.sh'.split())
             subprocess.run('ldapmodify -Y EXTERNAL -H ldapi:/// -f mod_ssl.ldif'.split())
         subprocess.run('systemctl restart slapd'.split())
-        subprocess.run('systemctl restart slapd'.split())
+        print('\n')
+        time.sleep(1)
+        subprocess.run('systemctl status slapd'.split())
     except BaseException as e:
         print(e)
 
 
 def run(command, type):
-    print(f"Installing Openldap {type} ", end='')
+    print("Installing Openldap ", type, end="")
     for i in range(4):
         print('.', end='')
         time.sleep(1)
@@ -45,7 +58,7 @@ def run(command, type):
 
 if num == '1':
     if distro == 'focal':
-        run('sudo -S bash openldap.server.installer.sh install', 'server')
+        run('sudo -S bash openldap.server.installer.sh install', 'sever')
     elif distro == 'bionic':
         run('sudo -S bash openldap.server.installer.sh install', 'server')
     elif distro == 'xenial':
