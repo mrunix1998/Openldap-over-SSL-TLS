7a7d06f366731b1b94282c07f64d9f923dd8bcab
mrunix1998 <mohammadsalehi31335@gmail.com>
2021-09-13 16:48:09 +0430

    update source code

--- README.md
+++ README.md
@@ -4,4 +4,13 @@ Auto install and configure openldap in ubuntu
 three distribution of ubuntu -> focal - bionic - xenial
 
 ## Run guid
-Just run the main.py with python command in linux or windows -> python main.py
+To run this project you need to some config based on your config that considered
+
+> $ sudo apt-get dist-upgrade \
+$ sudo apt-get update \
+$ sudo python3.8 main.py 
+
+in ldap.conf file: 
+> uri = is your ldap / ldaps url
+> base = is your ldap / ldaps domain name 
+> rootbinddn = is your distinguished name of uri
--- main.py
+++ main.py
@@ -6,16 +6,38 @@ from getpass import getpass
 print("""
     1 - OpenLdap server without SSL/TLS
     2 - OpenLdap client without SSL/TLS
-    3 - Openldap server with SSL/TLS
-    4 - OpenLdap client with SSL/TLS
+    3 - Generate SSL/TLS for openldap server ( Berfor use this section please install openldap with section 1 ) 
+    4 - Generate SSL/TLS for openldap client ( Berfor use this section please install openldap with section 2 )
 """)
 
 num = input("Enter Your choice : ")
 distro = input("Enter Your release of ubuntu [focal | bionic | xenial] : ")
 
 
-def generate_ssl(distro):
+def use_ssl(distro):
     try:
+        print("Generating self signed SSL for openldap-client")
+        if distro == 'focal':
+            os.popen('apt install -y nslcd nscd').read()
+            time.sleep(1)
+            os.popen('echo "ssl start_tls" >> /etc/nslcd.conf').read()
+            time.sleep(1)
+            os.popen('echo "tls_reqcert allow" >> /etc/nslcd.conf').read()
+            time.sleep(1)
+            subprocess.run('systemctl restart slapd'.split())
+        else :
+            os.popen('echo "TLS_REQCERT allow" >> /etc/ldap/ldap.conf').read()
+            time.sleep(2)
+            os.popen('cat ldap.conf > /etc/ldap.conf')
+            time.sleep(2)
+        print("Done")
+    except BaseException as e:
+        print(e)
+
+
+def generate_ssl():
+    try:
+        print("Generating self signed SSL for openldap-server")
         os.popen('echo "#TLS_CACERT    /etc/ssl/certs/ca-certificate" > /etc/ldap/ldap.conf').read()
         time.sleep(1)
         print("config set")
@@ -30,15 +52,15 @@ def generate_ssl(distro):
         time.sleep(1)
         subprocess.run('openssl x509 -in /etc/ssl/private/server.csr -out /etc/ssl/private/server.crt -req -signkey /etc/ssl/private/server.key -days 3650'.split())
         time.sleep(1)
-        subprocess.run('cp /etc/ssl/private/{server.key,server.crt} /etc/ssl/certs/ca-certificates.crt /etc/ldap/sasl2/'.split())
+        subprocess.run('cp /etc/ssl/private/server.key /etc/ssl/private/server.crt /etc/ssl/certs/ca-certificates.crt /etc/ldap/sasl2/'.split())
         time.sleep(1)
         subprocess.run('chown openldap. /etc/ldap/sasl2/server.key /etc/ldap/sasl2/server.crt /etc/ldap/sasl2/ca-certificates.crt'.split())
         time.sleep(1)
-        if distro == 'xenial':
-            subprocess.run('bash command.sh'.split())
-            subprocess.run('ldapmodify -Y EXTERNAL -H ldapi:/// -f mod_ssl.ldif'.split())
+        subprocess.run('bash add_ldaps.sh'.split())
+        time.sleep(2)
+        subprocess.run('ldapmodify -Y EXTERNAL -H ldapi:/// -f mod_ssl.ldif'.split())
+        time.sleep(2)
         subprocess.run('systemctl restart slapd'.split())
-        print('\n')
         time.sleep(1)
         subprocess.run('systemctl status slapd'.split())
     except BaseException as e:
@@ -58,7 +80,7 @@ def run(command, type):
 
 if num == '1':
     if distro == 'focal':
-        run('sudo -S bash openldap.server.installer.sh install', 'sever')
+        run('sudo -S bash openldap.server.installer.sh install', 'server')
     elif distro == 'bionic':
         run('sudo -S bash openldap.server.installer.sh install', 'server')
     elif distro == 'xenial':
@@ -75,42 +97,36 @@ elif num == '2':
 elif num == '3':
     if distro == 'focal':
         try:
-            run('sudo -S bash  openldap.sslserver.installer.sh install', 'server')
+            generate_ssl()
         except BaseException as e:
             print(e)
-        generate_ssl(distro)
     elif distro == 'bionic':
         try:
-            run('sudo -S bash  openldap.sslserver.installer.sh install', 'server')
+            generate_ssl()
         except BaseException as e:
             print(e)
-        generate_ssl(distro)
 
     elif distro == 'xenial':
         try:
-            run('sudo -S bash  openldap.sslserver.installer.sh install', 'server')
+            generate_ssl()
         except BaseException as e:
             print(e)
-        generate_ssl(distro)
 
 elif num == '4':
     if distro == 'focal':
         try:
-            run('sudo -S bash openldap.sslclient.installer.sh install', 'client')
+            use_ssl(distro)
         except BaseException as e:
             print(e)
-        generate_ssl(distro)
 
     elif distro == 'bionic':
         try:
-            run('sudo -S bash openldap.sslclient.installer.sh install', 'client')
+            use_ssl(distro)
         except BaseException as e:
             print(e)
-        generate_ssl(distro)
 
     elif distro == 'xenial':
         try:
-            run('sudo -S bash openldap.sslclient.installer.sh install', 'client')
+            use_ssl(distro)
         except BaseException as e:
             print(e)
-        generate_ssl(distro)
